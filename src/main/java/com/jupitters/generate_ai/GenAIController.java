26e6ced3b74691b67e217c32f0a80d4867bfa5fe
jupitters <sampaiobale@gmail.com>
2026-03-05 10:52:08 -0300

    controller

--- src/main/java/com/jupitters/generate_ai/GenAIController.java
+++ src/main/java/com/jupitters/generate_ai/GenAIController.java
@@ -1,6 +1,8 @@
 package com.jupitters.generate_ai;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenAIController {
     private final ChatService chatService;
 
-    public String getReseponse(String prompt){
+    @GetMapping("/ask-ai")
+    public String getReseponse(@RequestParam  String prompt){
         return chatService.getResponse(prompt);
     }
 }

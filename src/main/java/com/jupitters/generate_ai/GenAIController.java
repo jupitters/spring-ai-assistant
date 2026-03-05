668d57dd8b9f4c61d0d5736c97a4c476bec559f2
jupitters <sampaiobale@gmail.com>
2026-03-05 10:50:57 -0300

    controller

--- src/main/java/com/jupitters/generate_ai/GenAIController.java
+++ src/main/java/com/jupitters/generate_ai/GenAIController.java
@@ -1,4 +1,14 @@
 package com.jupitters.generate_ai;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class GenAIController {
+    private final ChatService chatService;
+
+    public String getReseponse(String prompt){
+        return chatService.getResponse(prompt);
+    }
 }

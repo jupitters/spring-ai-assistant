5c55e1b38512639c68dc77a4f23e824cf2205e1d
jupitters <sampaiobale@gmail.com>
2026-03-05 12:34:45 -0300

    service

--- src/main/java/com/jupitters/generate_ai/ChatService.java
+++ src/main/java/com/jupitters/generate_ai/ChatService.java
@@ -2,6 +2,8 @@ package com.jupitters.generate_ai;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +13,17 @@ public class ChatService {
 
     public String getResponse(String prompt) {
         return chatModel.call(prompt);
+    }
+
+    public String getResponseOptions(String prompt) {
+        return chatModel.call(
+                new Prompt(
+                        "Generate the names of 5 famous pirates",
+                        OpenAiChatOptions.builder()
+                                .model("gpt-4-o")
+                                .temperature(0.4)
+                                .build()
+                )
+        );
     }
 }

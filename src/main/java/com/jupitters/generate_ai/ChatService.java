bee32a317d74f545560600b9bf843241511ba424
jupitters <sampaiobale@gmail.com>
2026-03-05 10:46:04 -0300

    dependencies

--- src/main/java/com/jupitters/generate_ai/ChatService.java
+++ src/main/java/com/jupitters/generate_ai/ChatService.java
@@ -1,9 +1,11 @@
 package com.jupitters.generate_ai;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
     private final ChatModel chatModel;
 }

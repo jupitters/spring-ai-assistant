493bccc256f4531078cfe95ef3cf70014a8a1324
jupitters <sampaiobale@gmail.com>
2026-03-05 10:25:21 -0300

    init

---
+++ src/main/java/com/jupitters/generate_ai/GenerateAiApplication.java
@@ -0,0 +1,13 @@
+package com.jupitters.generate_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jupitters/generate_ai/GenerateAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupitters.generate_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

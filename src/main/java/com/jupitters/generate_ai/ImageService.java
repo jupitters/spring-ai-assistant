d44c2be48ac2f6cd90525e515685afff91d33f65
jupitters <sampaiobale@gmail.com>
2026-03-05 12:45:27 -0300

    image service

--- src/main/java/com/jupitters/generate_ai/ImageService.java
+++ src/main/java/com/jupitters/generate_ai/ImageService.java
@@ -1,4 +1,21 @@
 package com.jupitters.generate_ai;
 
-public class ImageService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService{
+    private final OpenAiImageModel openAiImageModel;
+
+    public ImageResponse generateImage(String prompt){
+        ImageResponse imageResponse = openAiImageModel.call(
+                new ImagePrompt(prompt)
+        );
+
+        return imageResponse;
+    }
 }

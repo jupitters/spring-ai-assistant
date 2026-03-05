246d1a1f51cabd83ef4d9a9e74c815d8da4e5ca6
jupitters <sampaiobale@gmail.com>
2026-03-05 10:43:14 -0300

    chat service

---
+++ src/main/java/com/jupitters/generate_ai/ChatService.java
@@ -0,0 +1,9 @@
+package com.jupitters.generate_ai;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+    private final ChatModel chatModel;
+}

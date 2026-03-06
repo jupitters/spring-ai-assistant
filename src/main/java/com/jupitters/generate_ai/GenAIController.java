b860052527a0992e981b91716c43859376efe314
jupitters <sampaiobale@gmail.com>
2026-03-06 17:31:09 -0300

    changind model

--- src/main/java/com/jupitters/generate_ai/GenAIController.java
+++ src/main/java/com/jupitters/generate_ai/GenAIController.java
@@ -2,6 +2,7 @@ package com.jupitters.generate_ai;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImageResponse;
@@ -14,22 +15,27 @@ import java.io.IOException;
 import java.util.Objects;
 
 @RestController
-@RequiredArgsConstructor
 public class GenAIController {
-    private final ChatService chatService;
-    private final ImageService imageService;
+    private final ChatClient chatClient;
+
+    public GenAIController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
 
     @GetMapping("/ask-ai")
-    public String getResponse(@RequestParam String prompt){
-        return chatService.getResponse(prompt);
+    public String getResponse(@RequestParam String message){
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
     }
 
-    @GetMapping("/generate-image")
-    public void generateImage(HttpServletResponse response, @RequestParam String prompt) throws IOException {
-        ImageResponse image = imageService.generateImage(prompt);
-
-        String url = Objects.requireNonNull(image.getResult()).getOutput().getUrl();
-
-        response.sendRedirect(url);
-    }
+//    @GetMapping("/generate-image")
+//    public void generateImage(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+//        ImageResponse image = imageService.generateImage(prompt);
+//
+//        String url = Objects.requireNonNull(image.getResult()).getOutput().getUrl();
+//
+//        response.sendRedirect(url);
+//    }
 }

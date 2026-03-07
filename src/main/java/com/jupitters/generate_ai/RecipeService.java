590542a1d30d036b4b277f2cc2a0eb092f640cfd
jupitters <sampaiobale@gmail.com>
2026-03-07 12:42:49 -0300

    recipe service

--- src/main/java/com/jupitters/generate_ai/RecipeService.java
+++ src/main/java/com/jupitters/generate_ai/RecipeService.java
@@ -1,4 +1,34 @@
 package com.jupitters.generate_ai;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class RecipeService {
+    private final ChatModel chatModel;
+
+    public String createRecipe(String ingredients, String cuisine, String dietaryRestrictions){
+        String template = """
+                I want to create a recipe using the following ingredients: {ingredients}.
+                The cuisine type i prefer is {cuisine}.
+                Consider the following dietary restrictions: {dietaryRestrictions}.
+                Provide me with a detailed recipe including title, list of ingredients and cooking instructions.
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Map<String, Object> params = Map.of(
+                "ingredients", ingredients,
+                "cuisine",cuisine,
+                "dietaryRestrictions", dietaryRestrictions
+        );
+
+        Prompt prompt = promptTemplate.create(params);
+        chatModel.call(prompt).getResult().getOutput();
+    }
 }
